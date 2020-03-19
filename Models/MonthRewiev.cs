5e8bb367b32bfcf70ba6e341381f9f780610c92a
daiver2008@gmail.com <daiver2008@gmail.com>
2020-03-19 08:46:45 +0300

    DB_First! Перенесена структура БД в код программы

---
+++ Models/MonthRewiev.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Review.Models
+{
+    public partial class MonthRewiev
+    {
+        public int Id { get; set; }
+        public int IndexStation { get; set; }
+        public DateTime Date { get; set; }
+        public double? Tmid { get; set; }
+        public double? Tmax { get; set; }
+        public double? Tmin { get; set; }
+        public short? Tg { get; set; }
+        public double? Rnight { get; set; }
+        public double? Rday { get; set; }
+        public short? Snow { get; set; }
+        public short? BlastMaxSpeed { get; set; }
+        public bool? Snowstorm { get; set; }
+        public bool? Mist { get; set; }
+        public bool? Hoarfrost { get; set; }
+        public bool? GlazedFrost { get; set; }
+        public bool? ThunderStorm { get; set; }
+        public bool? ComplexFrost { get; set; }
+    }
+}
---
+++ Models/evgenmr_ReviewContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Review.Models
+{
+    public partial class evgenmr_ReviewContext : DbContext
+    {
+        public evgenmr_ReviewContext()
+        {
+        }
+
+        public evgenmr_ReviewContext(DbContextOptions<evgenmr_ReviewContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<MonthRewiev> MonthRewiev { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=sql.freeasphost.net\\MSSQL2016;Database=evgenmr_Review;User ID=evgenmr;Password=Vehvfycr2020;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MonthRewiev>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.ComplexFrost).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.Property(e => e.GlazedFrost).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Hoarfrost).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Mist).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Rday).HasColumnName("RDay");
+
+                entity.Property(e => e.Rnight).HasColumnName("RNight");
+
+                entity.Property(e => e.Snowstorm).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Tg).HasColumnName("TG");
+
+                entity.Property(e => e.ThunderStorm).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Tmax).HasColumnName("TMax");
+
+                entity.Property(e => e.Tmid).HasColumnName("TMid");
+
+                entity.Property(e => e.Tmin).HasColumnName("TMin");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
